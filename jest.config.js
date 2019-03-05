64ebe9ea4bbf22658500ddcb300e1ad46e864db6
Artem Slepets <49h4DhPERFECT@gmail.com>
2019-03-05 21:18:12 +0200

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    }
+  },
+  "roots": [
+    "<rootDir>/src"
+  ],
+  "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
+  ],
+};
